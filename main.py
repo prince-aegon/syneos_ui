de01c7f235a9f0df3bc1a3f16fc6989f2f78f5f1
Sarthak Jha <jhasarthak123@gmail.com>
2024-04-17 00:57:29 +0530

    rag added to ui

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
+import pandas as pd
 import gradio as gr
 import openai
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 from sentence_transformers import SentenceTransformer
 from pinecone import Pinecone
 
-
-# Define the Python function that takes three inputs and returns a result
-
 llm_models = {"GPT-3.5": "gpt-3.5-turbo-0125", "GPT-4": "gpt-4-turbo"}
 
 INDEX_NAME = "mtp"
 
-# Initialize Pinecone client
 pc = Pinecone(api_key='47703291-8727-4f0c-a780-571542ece3a3')
 
 index = pc.Index(
     host='https://mtp-9iwsr1u.svc.aped-4627-b74a.pinecone.io'
-    )
+)
+
 
 def RAG(prompt_text):
     model = SentenceTransformer("all-mpnet-base-v2")
@@ -24,143 +22,100 @@ def RAG(prompt_text):
     embeddings = model.encode(prompt_text)
     context = gradio_interface(embeddings)
     return context
-    # query = perform_rag(prompt_text,context)
 
-
-# def perform_rag(context, top_context):
-#   # Load your chosen RAG model and tokenizer (replace placeholders)
-#   model = SentenceTransformer("all-mpnet-base-v2")
-
-#   tokenizer = AutoTokenizer.from_pretrained(model)
-# #   model = AutoModelForSeq2SeqLM.from_pretrained(model)
-
-#   # Prepare RAG inputs (replace with your processing steps)
-#   input_ids = tokenizer(context, top_context, return_tensors="pt")["input_ids"]
-
-#   # Generate text using your RAG model (replace with your logic)
-#   output = model.generate(**input_ids)
-#   generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
-
-#   return generated_text
-
-import pandas as pd
 
 df = pd.read_csv("Data.csv", encoding='cp1252')
 
 
 def gradio_interface(context):
-    # print("Context Is :")
-    # print()
-    top_k = 1
-    # Perform retrieval using Pinecone based on the context
+
+    top_k = 2
     vec = context.tolist()
-
-    # print(type(vec))
-    # print(vec)
-
-    # print("Embeddings are ")
     embeddings = index.query(
-    vector=vec,
-    top_k=top_k,
-    include_values=True)
-
-    # print(embeddings)
+        vector=vec,
+        top_k=top_k,
+        include_values=True)
     all_values = [match['id'] for match in embeddings['matches']]
-
-  # Var to store generated text of top contexts
-
-    ret = "From Pinecone: You may consider as an example of a similar patients with the inclusion, exclusion, and eligibility below to make your decision about the eligibility:"
-    # Loop through top k results and perform RAG for each
-    for j in  range(top_k):
-        print("Id is ", all_values[j] )
+    ret = "\nHere is the first set of criteria, patient profile and result on eligibility:"
+    for j in range(top_k):
+        print("Id is ", all_values[j])
 
         i = int(all_values[j])
         r = ""
-        r = "\n Inclusion: "
+        r = "\n Inclusion Criteria for the trial is "
         r += "\n"
         r += df['ï»¿inclusion_criteria'][i]
 
-        r += "\n Exclusion: "
+        r += "\n Exclusion Criteria for the trial is "
         r += "\n"
-        r+= df['exclusion_criteria'][i]            
+        r += df['exclusion_criteria'][i]
 
-        r += "\n Patient Profile: "
+        r += "\n Patient Profile is "
         r += "\n"
         r += df['patient_profiles'][i]
 
-        r += "\n Eligibility: "
+        r += "\n The eligibility of the patient for the clinical trial is: "
         r += "\n"
         r += df['target'][i]
-        
-        if j!= top_k-1:
-            r+=" \n Here is another patient profile from Pinecone."
+
+        if j != top_k-1:
+            r += " \n Here is another patient profile: "
 
         ret += r
     return ret
 
 
-def process_text(input1, input2, input3, key, model_in, temp_in):
-    # openai.api_key = "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"
+def process_text(input1, input2, input3, key, model_in, temp_in, rag_check):
+    print("RAG : ", rag_check)
+    openai.api_key = key
     curr_string = "You are an oncologist. If for a clinical trial, the Inclusion Criteria is :\n" + \
         input2 + "\n" + \
         "and the Exclusion Criteria is : " + input3 + "\n" + \
         "and the Patient Profile is :" + \
         input1 + \
-        ".\n Then given this information determine if the patient is eligible for the trial. Give a binary answer Eligible/Not Eligible as the response. In the response please do not add any extra text, just the response Eligible or Not Eligible"
+        ".\n Then given this information determine if the patient is eligible for the trial. Give a binary answer Eligible/Not Eligible as the response. In the response please do not add any extra text, just the response Eligible or Not Eligible. After giving the verdict in one line, from the next line give the reasons for giving the verdict as eligible or not eligible"
+    no_rag_case = curr_string
+    curr_string += "\nTo determine the eligibility, you may consider as an example, some similar patients with inclusion criteria, exclusion criteria, and a verdict on its eligibility below to help you better make your decision about the eligibility:\n"
 
-    # TO perform RAG
-
-    prompt = "You are an oncologist. You job is to check the below mentioned patient profile against an inclusion and exclusion criteria and tell whether the patient is eligible for the study or not."
     query = RAG(curr_string)
-    prompt += query
+    curr_string += query
 
-    prompt += "\n Inclusion:"
-    prompt +="\n"
-    prompt += input2
-
-    prompt += "\n Exclusion:"
-    prompt +="\n"
-    prompt += input3
-
-    prompt += "\n Patient Profile:"
-    prompt +="\n"
-    prompt += input1
-    
-
-    prompt +="Please don't add any extra text. Just reply Eligible/ Not Eligible "
-    # prompt_parts = curr_string
-    # print(prompt_parts)
+    curr_string = curr_string if rag_check else no_rag_case
+    print(curr_string)
 
     response = openai.ChatCompletion.create(
         model=llm_models[model_in],
         messages=[{"role": "system", "content": "You are a helpful assistant and acting as an oncologist"}, {
-            "role": "user", "content": query}],
+            "role": "user", "content": curr_string}],
         temperature=temp_in
     )
     print(response)
-    return (response["choices"][0]["message"]["content"])
+    llm_reasoning = ""
+    for point in response["choices"][0]["message"]["content"].split("\n")[1:]:
+        llm_reasoning += point + "\n"
+    return (response["choices"][0]["message"]["content"].split("\n")[0], llm_reasoning)
 
 
-# Create input boxes for the three inputs
 input_box1 = gr.Textbox(label="Patient Profile")
 input_box2 = gr.Textbox(label="Inclusion Criteria")
 input_box3 = gr.Textbox(label="Exclusion Criteria")
 input_box4 = gr.Textbox(label="API Key")
 
-output_box = gr.Textbox(label="Verdict")
-# Create the Gradio interface
+output_box1 = gr.Textbox(label="Verdict")
+output_box2 = gr.Textbox(label="LLM Reason")
+
 image_path = "logo.png"
 with gr.Blocks() as demo:
-    # gr.Image("logo.png", height="20px", show_label=False,
-    #          show_download_button=False, show_share_button=False)
     big_block = gr.HTML("""
     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Syneos_Health_logo.svg/1200px-Syneos_Health_logo.svg.png" alt="logo" width="150" height="150">
     """)
     gr.Interface(fn=process_text,
                  inputs=[input_box1, input_box2, input_box3, input_box4, gr.Dropdown(
                         ["GPT-3.5", "GPT-4"], label="LLM Model", info="Select a LLM model"), gr.Slider(0, 1, value=0.9, label="Temperature", info="Select a Temperature"),
+                     gr.Checkbox(label="Apply RAG",
+                                 info="Check the following to use RAG", value=True),
                  ],
-                 outputs=output_box,
+                 outputs=[output_box1, output_box2],
                  title="Predict Patient Eligibility",
                  description="Enter Patient Profile and Inclusion and Exclusion criteria to check Eligibility of patient",
                  allow_flagging="never",
